b64bf3abdc0545296f01ca824851d88341614ae4
teohealy <loyshastegan@mail.ru>
2024-01-22 16:00:55 +0300

    Commit for filters

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -4,15 +4,14 @@ public class User {
 
     private int id;
     private String name;
-    private int age;
 
-    public User(int id, String name, int age) {
+    public User(int id, String name) {
         this.id = id;
         this.name = name;
-        this.age = age;
     }
 
-    public User(){}
+    public User() {
+    }
 
     public int getId() {
         return id;
@@ -30,19 +29,11 @@ public class User {
         this.name = name;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     @Override
     public String toString() {
         return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 '}';
     }
 }
--- src/main/java/servlet/GetIndexPageServlet.java
+++ src/main/java/servlet/GetIndexPageServlet.java
@@ -1,47 +1,28 @@
 package servlet;
 
-import model.User;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 
-@WebServlet("/users")
+@WebServlet("/filter")
 public class GetIndexPageServlet extends HttpServlet {
 
-    private static String index = "/users.jsp";
-
-    private Map<Integer, User> userMap;
+    private static final String index = "/index.jsp";
 
     @Override
-    public void init() throws ServletException{
-        System.out.println("SERVLET IS INIT");
-
-        final Object userMap = getServletContext().getAttribute("users");
-
-        if (userMap == null || !(userMap instanceof ConcurrentHashMap)){
-            throw new IllegalStateException("Данные не проицициализированы");
-        }else{
-            this.userMap = (ConcurrentHashMap<Integer, User>) userMap;
-        }
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(index).forward(req, resp);
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        req.setAttribute("users", userMap.values());
-        req.getRequestDispatcher(index).forward(req, res);
-    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String data = req.getParameter("data");
+        resp.sendRedirect(index);
 
-
-
-    @Override
-    public void destroy(){
-        System.out.println("SERVLET IS DESTROY");
+        System.out.println(data);
     }
 }
